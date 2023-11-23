23d329b0be601096443546398b490971dbfe81f6
Paulo Abreu <paulo.abreu@clustercube.com>
2023-11-23 21:54:44 +0000

    Update seeds with countries and cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,16 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+countries = ['Portugal', 'Spain', 'France']
+cities = {
+  'Portugal' => ['Lisbon', 'Porto', 'Coimbra', 'Faro'],
+  'Spain' => ['Madrid', 'Barcelona', 'Seville', 'Valencia'],
+  'France' => ['Paris', 'Lyon', 'Marseille', 'Nice']
+}
+
+countries.each do |country_name|
+  country = Country.create!(name: country_name)
+  cities[country_name].each do |city_name|
+    City.create!(name: city_name, country: country)
+  end
+end
